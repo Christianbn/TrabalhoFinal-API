2eb80643c1b55ba0b664ade7ac6a7e7545f43a67
PauloPetersen <paulo.g.petersen@hotmail.com>
2022-10-24 20:17:39 -0300

    Cliente Documentação Swagger

--- src/main/java/com/example/tf/controller/ClienteController.java
+++ src/main/java/com/example/tf/controller/ClienteController.java
@@ -24,6 +24,10 @@ import com.example.tf.exception.CpfException;
 import com.example.tf.exception.EmailException;
 import com.example.tf.service.ClienteService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping("/api/cliente")
 public class ClienteController {
@@ -32,6 +36,14 @@ public class ClienteController {
 	ClienteService clienteService;
 	
 	@GetMapping
+	@ApiOperation(value="Lista todos os clientes", notes="Listagem de clientes")
+	@ApiResponses(value= {
+	@ApiResponse(code=200, message="Retorna todos os clientes"),
+	@ApiResponse(code=401, message="Erro de autenticação"),
+	@ApiResponse(code=403, message="Não há permissão para acessar o recurso"),
+	@ApiResponse(code=404, message="Recurso não encontrado"),
+	@ApiResponse(code=505, message="Exceção interna da aplicação"),})
+	
 	public ResponseEntity<List<Cliente>> findAll() {
 		List<Cliente> cliente = clienteService.findAll();
 		if(cliente.isEmpty()) {
@@ -42,6 +54,14 @@ public class ClienteController {
 	} 
 	
 	@GetMapping("/{id}")
+	@ApiOperation(value="Retorna um cliente", notes="Cliente")
+	@ApiResponses(value= {
+	@ApiResponse(code=200, message="Retorna um cliente"),
+	@ApiResponse(code=401, message="Erro de autenticação"),
+	@ApiResponse(code=403, message="Não há permissão para acessar o recurso"),
+	@ApiResponse(code=404, message="Recurso não encontrado"),
+	@ApiResponse(code=505, message="Exceção interna da aplicação"),})
+	
 	public ResponseEntity<Optional<Cliente>> findById(@PathVariable Long id) {
 		Optional<Cliente> cliente = clienteService.findById(id);
 		if (cliente.isPresent()) {
@@ -52,6 +72,14 @@ public class ClienteController {
 	}
 	
 	@PostMapping
+	@ApiOperation(value="Insere os dados de um cliente", notes="Inserir Cliente")
+	@ApiResponses(value= {
+	@ApiResponse(code=201, message="Cliente adicionado"),
+	@ApiResponse(code=401, message="Erro de autenticação"),
+	@ApiResponse(code=403, message="Não há permissão para acessar o recurso"),
+	@ApiResponse(code=404, message="Recurso não encontrado"),
+	@ApiResponse(code=505, message="Exceção interna da aplicação"),})
+	
 	public ResponseEntity<Cliente> PostItemPedido(@Valid @RequestBody ClienteDTO_POST clienteDTO) throws EmailException, CpfException {
 		Cliente clienteTemp = clienteService.PostCliente(clienteDTO);
 		if (clienteTemp == null) {
@@ -63,6 +91,14 @@ public class ClienteController {
 	}
 	
 	@PutMapping("/{id}")
+	@ApiOperation(value="Atualiza dados de um cliente", notes="Atualizar Cliente")
+	@ApiResponses(value= {
+	@ApiResponse(code=200, message="Cliente atualizado"),
+	@ApiResponse(code=401, message="Erro de autenticação"),
+	@ApiResponse(code=403, message="Não há permissão para acessar o recurso"),
+	@ApiResponse(code=404, message="Recurso não encontrado"),
+	@ApiResponse(code=505, message="Exceção interna da aplicação"),})
+	
 	public ResponseEntity<Optional<Cliente>> Putcliente(@Valid @RequestBody Cliente cliente, @PathVariable Long id) {
 		Optional<Cliente> clienteTemp = clienteService.PutCliente(cliente, id);
 		if (!clienteTemp.isPresent()) {
@@ -74,6 +110,15 @@ public class ClienteController {
 	}
 	
 	@DeleteMapping("/{id}")
+	@ApiOperation(value="Remove um cliente", notes="Remover Cliente")
+	@ApiResponses(value= {
+	@ApiResponse(code=200, message="Cliente removido"),
+	@ApiResponse(code=204, message="Sem conteúdo"),
+	@ApiResponse(code=401, message="Erro de autenticação"),
+	@ApiResponse(code=403, message="Não há permissão para acessar o recurso"),
+	@ApiResponse(code=404, message="Recurso não encontrado"),
+	@ApiResponse(code=505, message="Exceção interna da aplicação"),})
+	
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		if (clienteService.DeleteCliente(id)) {
 			return ResponseEntity.noContent().build();
